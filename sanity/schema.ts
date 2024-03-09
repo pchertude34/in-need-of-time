60d47f33acf1b586a6b19ebe51a2c257f20ac500
Payton Chertude <p.chertude34@gmail.com>
2024-03-09 10:30:37 -0800

    import sanity schemas from the-path-v2

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,5 +1,7 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import providerSchema from './schemas/provider'
+import serviceTypeSchema from './schemas/serviceType'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [providerSchema, serviceTypeSchema],
 }
