9c90844d502c577cc2f519c45c5e2c1b4db74fcc
Payton Chertude <p.chertude34@gmail.com>
2024-03-26 17:31:03 -0700

    create google maps hook

---
+++ app/_hooks/useGoogleMaps.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { loadGoogleMapsApi } from "@/lib/loadGoogleMapsApi";
+import { NEXT_PUBLIC_GOOGLE_API_KEY } from "@/env";
+
+export class AuthError extends Error {}
+
+export function useLoadGoogleMaps() {
+  const [isLoadingMaps, setIsLoadingMaps] = useState(true);
+  const [mapsError, setMapsError] = useState<string | null>(null);
+  const [googleMaps, setGoogleMaps] = useState<
+    typeof window.google.maps | null
+  >(null);
+
+  useEffect(() => {
+    loadGoogleMapsApi({
+      locale: "en-US",
+      apiKey: NEXT_PUBLIC_GOOGLE_API_KEY,
+    }).then(
+      (maps) => {
+        setIsLoadingMaps(false);
+        setMapsError(null);
+        setGoogleMaps(maps);
+      },
+      (err: AuthError | Error) => {
+        setMapsError(
+          err instanceof AuthError ? "Authentication error" : err.message,
+        );
+        setIsLoadingMaps(false);
+      },
+    );
+  });
+
+  return { isLoadingMaps, mapsError, googleMaps };
+}
