4201adb05da90140d10407701f56eae48d789e6d
Payton Chertude <p.chertude34@gmail.com>
2024-04-15 20:38:59 -0700

    address card ui

--- app/(client)/the-path/page.tsx
+++ app/(client)/the-path/page.tsx
@@ -1,15 +1,24 @@
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function ThePathPage() {
   return (
-    <div className="container mx-auto px-4">
-      <Card className="p-6">
+    <div className="container mx-auto mt-10">
+      <Card className="space-y-6 p-6">
         <div>
           <h2 className="text-2xl font-medium">Choose Your Location</h2>
           <p>Type an address for yourself, or someone you'd like to assist</p>
         </div>
-        <div></div>
+        <div>
+          <Label htmlFor="address">Address</Label>
+          <Input id="address" />
+        </div>
+        <div>
+          <p className="mb-4">Or use your current location:</p>
+          <Button>Use Current Location</Button>
+        </div>
       </Card>
     </div>
   );
