f625c1faae06c7b01a3772d2062509f72128b1cf
Payton Chertude <p.chertude34@gmail.com>
2024-05-27 09:49:53 -0700

    fix build issue

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,11 +8,7 @@ import { Button } from "@/components/ui/button";
 import { Collapsible, CollapsibleTrigger, CollapsibleContent } from "@/components/ui/collapsible";
 import { cn } from "@/lib/utils";
 
-export const navigation = [
-  { name: "Home", href: "/", current: true },
-  { name: "About", href: "/about", current: false },
-  { name: "Contact", href: "/contact", current: false },
-];
+export const navigation = [{ name: "Home", href: "/" }];
 
 export default function Header() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -43,6 +39,7 @@ export default function Header() {
               <div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
                 {navigation.map((item) => (
                   <Link
+                    key={item.href}
                     className={cn("inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium", {
                       "border-emerald-500 text-gray-900": pathname === item.href,
                       "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700":
@@ -79,6 +76,7 @@ export default function Header() {
             {navigation.map((item) => (
               <CollapsibleTrigger key={`mobile-menu${item.name}`} asChild>
                 <Link
+                  key={item.href}
                   href={item.href}
                   className={cn("block border-l-4 py-2 pl-3 pr-4 text-base font-medium", {
                     "border-emerald-500 bg-emerald-50 text-emerald-700": pathname === item.href,
