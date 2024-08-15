a68087635c5ab36d22422424a6c1075aa2e50fed
Payton Chertude <p.chertude34@gmail.com>
2024-08-15 07:28:54 -0700

    center header

--- components/Header.tsx
+++ components/Header.tsx
@@ -37,8 +37,8 @@ export default function Header() {
     >
       <>
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:p-5">
-          <div className="flex justify-between">
-            <div className="flex">
+          <div className="flex">
+            <div className="flex flex-1 items-center">
               <div className="flex flex-shrink-0 items-center">
                 <img
                   className="block h-8 w-auto lg:hidden"
@@ -51,32 +51,33 @@ export default function Header() {
                   alt="Your Company"
                 />
               </div>
-              <div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
-                <NavigationMenu>
-                  <NavigationMenuList>
-                    <NavigationMenuItem>
-                      <Link href="/" legacyBehavior passHref>
-                        <NavigationMenuLink
-                          data-active={pathname === "/" ? true : null}
-                          className={navigationMenuTriggerStyle()}
-                        >
-                          <HomeIcon className="mr-2 h-4 w-4" />
-                          Home
-                        </NavigationMenuLink>
-                      </Link>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem>
-                      <Link href="/search" legacyBehavior passHref>
-                        <NavigationMenuLink
-                          data-active={pathname === "/search" ? true : null}
-                          className={navigationMenuTriggerStyle()}
-                        >
-                          <SearchIcon className="mr-2 h-4 w-4" />
-                          Search
-                        </NavigationMenuLink>
-                      </Link>
-                    </NavigationMenuItem>
-                    {/* <NavigationMenuItem>
+            </div>
+            <div className="hidden flex-1 items-center justify-center sm:flex">
+              <NavigationMenu>
+                <NavigationMenuList>
+                  <NavigationMenuItem>
+                    <Link href="/" legacyBehavior passHref>
+                      <NavigationMenuLink
+                        data-active={pathname === "/" ? true : null}
+                        className={navigationMenuTriggerStyle()}
+                      >
+                        <HomeIcon className="mr-2 h-4 w-4" />
+                        Home
+                      </NavigationMenuLink>
+                    </Link>
+                  </NavigationMenuItem>
+                  <NavigationMenuItem>
+                    <Link href="/search" legacyBehavior passHref>
+                      <NavigationMenuLink
+                        data-active={pathname === "/search" ? true : null}
+                        className={navigationMenuTriggerStyle()}
+                      >
+                        <SearchIcon className="mr-2 h-4 w-4" />
+                        Search
+                      </NavigationMenuLink>
+                    </Link>
+                  </NavigationMenuItem>
+                  {/* <NavigationMenuItem>
                       <Link href="/providers" legacyBehavior passHref>
                         <NavigationMenuLink
                           data-active={pathname === "/providers" ? true : null}
@@ -86,35 +87,21 @@ export default function Header() {
                         </NavigationMenuLink>
                       </Link>
                     </NavigationMenuItem> */}
-                    <NavigationMenuItem>
-                      <Link href="/about" legacyBehavior passHref>
-                        <NavigationMenuLink
-                          data-active={pathname === "/about" ? true : null}
-                          className={navigationMenuTriggerStyle()}
-                        >
-                          <BookOpenIcon className="mr-2 h-4 w-4" />
-                          About
-                        </NavigationMenuLink>
-                      </Link>
-                    </NavigationMenuItem>
-                  </NavigationMenuList>
-                </NavigationMenu>
-                {/* {navigation.map((item) => (
-                  <Link
-                    key={item.href}
-                    className={cn("inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium", {
-                      "border-emerald-500 text-gray-900": pathname === item.href,
-                      "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700":
-                        pathname !== item.href,
-                    })}
-                    href={item.href}
-                  >
-                    {item.name}
-                  </Link>
-                ))} */}
-              </div>
+                  <NavigationMenuItem>
+                    <Link href="/about" legacyBehavior passHref>
+                      <NavigationMenuLink
+                        data-active={pathname === "/about" ? true : null}
+                        className={navigationMenuTriggerStyle()}
+                      >
+                        <BookOpenIcon className="mr-2 h-4 w-4" />
+                        About
+                      </NavigationMenuLink>
+                    </Link>
+                  </NavigationMenuItem>
+                </NavigationMenuList>
+              </NavigationMenu>
             </div>
-            <div className="hidden sm:ml-6 sm:flex sm:items-center">
+            <div className="hidden flex-1 items-center justify-end sm:flex">
               <Button variant="primary" asChild>
                 <Link href="/find">
                   Find a Provider <ArrowRightIcon className="ml-2 h-3 w-3" />
