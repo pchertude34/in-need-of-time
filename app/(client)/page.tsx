f1a18b0c56d18c3cfafa4c93cecbd71e1a26b679
Payton Chertude <p.chertude34@gmail.com>
2024-08-18 10:57:08 -0700

    udpate some spacing for hero

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -4,9 +4,9 @@ export default function Home() {
       {/* Hero Section */}
       <div className="bg-hero-image before mt-3 overflow-hidden rounded-3xl bg-cover bg-center">
         <div className="bg-primary-500/40 h-[486px] w-full bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))] from-black/30 to-black/50">
-          <div className="flex flex-col items-center justify-center px-64 pt-32">
+          <div className="flex flex-col items-center justify-center px-60 pt-32">
             <h1 className="text-center text-5xl font-bold leading-snug tracking-tight text-white">
-              Build a great community by helping each and everyone in service
+              Building a great community by helping each and everyone in service
               <span className="text-secondary-400">.</span>
             </h1>
             <p className="mt-3 text-center text-lg text-white">
