14ec8583f7c3157bd80af0f4e32ed63448439316
Payton Chertude <p.chertude34@gmail.com>
2024-08-18 11:14:29 -0700

    responsive hero img size

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <main className="container">
       {/* Hero Section */}
       <div className="bg-hero-image before mt-3 overflow-hidden rounded-3xl bg-cover bg-center">
-        <div className="bg-primary-500/40 h-[486px] w-full bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))] from-black/30 to-black/50">
+        <div className="bg-primary-500/40 h-[410px] w-full bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))] from-black/30 to-black/50 sm:h-[486px]">
           <div className="p flex flex-col items-center justify-center pt-16 sm:pt-24 md:px-10 md:pt-32 lg:px-20 xl:px-60">
             <div className="px-3 md:px-0">
               <h1 className="text-center text-3xl font-bold leading-snug tracking-tight text-white sm:text-4xl md:text-5xl">
