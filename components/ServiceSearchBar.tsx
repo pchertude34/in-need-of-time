b4b889e63522c31ae398f061f3271cc86075eeda
Payton Chertude <p.chertude34@gmail.com>
2024-08-19 08:32:54 -0700

    add input group component

--- components/ServiceSearchBar.tsx
+++ components/ServiceSearchBar.tsx
@@ -1,14 +1,29 @@
 import React from "react";
-import { MapPinIcon } from "@heroicons/react/24/outline";
+import { MapPinIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { LocateFixed } from "lucide-react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
+import { Separator } from "./ui/separator";
+import { Combobox } from "./ui/combobox";
+import { InputGroup, InputLeftElement, InputRightElement } from "./ui/input-group";
 
 export function ServiceSearchBar() {
   return (
     <div className="rounded-full border border-slate-200 px-3 py-4">
-      <div className="flex items-center">
-        <div className="relative">
+      <div className="flex items-stretch">
+        {/* Location input */}
+        <InputGroup>
+          <InputLeftElement>
+            <MapPinIcon className="h-4 w-4 text-slate-500" />
+          </InputLeftElement>
+          <Input className=" border-none px-10" type="text" placeholder="Enter your location or ZIP" />
+          <InputRightElement>
+            <Button variant="text-primary" size="text">
+              <LocateFixed className="h-4 w-4" />
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+        {/* <div className="relative">
           <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
             <span className="text-slate-500">
               <MapPinIcon className="h-4 w-4" />
@@ -20,6 +35,24 @@ export function ServiceSearchBar() {
               <LocateFixed className="h-4 w-4" />
             </Button>
           </div>
+        </div> */}
+        {/*  */}
+        <div className="mx-3">
+          <Separator orientation="vertical" />
+        </div>
+        {/* Provider type selector */}
+        <div className="relative">
+          <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+            <span className="text-slate-500">
+              <MagnifyingGlassIcon className="h-4 w-4" />
+            </span>
+          </div>
+          <Combobox />
+          {/* <div className="absolute inset-y-0 right-0 flex items-center pr-3">
+            <Button variant="text-primary" size="text">
+              <LocateFixed className="h-4 w-4" />
+            </Button>
+          </div> */}
         </div>
       </div>
     </div>
---
+++ components/ui/input-group.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { cn } from "@/lib/utils";
+
+type InputGroupProps = {
+  children: React.ReactElement | React.ReactElement[];
+  className?: string;
+};
+
+export function InputGroup(props: InputGroupProps) {
+  const { children, className } = props;
+
+  return <div className={cn("relative", className)}>{children}</div>;
+}
+
+type InputLeftElementProps = {
+  children: React.ReactNode;
+  className?: string;
+};
+export function InputLeftElement(props: InputLeftElementProps) {
+  const { children, className } = props;
+
+  return (
+    <div className={cn("pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3", className)}>
+      {children}
+    </div>
+  );
+}
+
+type InputRightElementProps = {
+  children: React.ReactNode;
+  className?: string;
+};
+export function InputRightElement(props: InputRightElementProps) {
+  return (
+    <div className={cn("absolute inset-y-0 right-0 flex items-center pr-3", props.className)}>{props.children}</div>
+  );
+}
