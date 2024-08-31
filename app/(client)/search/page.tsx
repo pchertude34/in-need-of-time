70f5302b2a973d52af84c439f1f62ecb8b7093ef
Payton Chertude <p.chertude34@gmail.com>
2024-08-31 11:19:31 -0700

    tabs

--- app/(client)/search/page.tsx
+++ app/(client)/search/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { ProviderMap } from "./components/ProviderMap";
 import { ServiceSearchBar } from "@/components/ServiceSearchBar";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { Separator } from "@/components/ui/separator";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { ProviderResultCard } from "./components/ProviderResultCard";
 
 type SearchPageProps = {
@@ -17,8 +17,15 @@ export default function SearchPage(props: SearchPageProps) {
     <div>
       <ProviderMap>
         <div className="absolute flex h-[calc(100vh-88px)] w-full items-start overflow-hidden p-6">
-          <ScrollArea className="z-10 h-full w-[403px] rounded-2xl bg-white shadow-xl">
-            <div className="p-6">
+          <div className="z-10 flex max-h-full w-[403px] flex-col space-y-4 rounded-2xl bg-white p-6 shadow-xl">
+            <span className="font-bold text-secondary-500">10 results found</span>
+            <Tabs defaultValue="locations">
+              <TabsList className="grid w-full grid-cols-2">
+                <TabsTrigger value="locations">Locations</TabsTrigger>
+                <TabsTrigger value="others">Others</TabsTrigger>
+              </TabsList>
+            </Tabs>
+            <ScrollArea className="max-h-[500px]">
               <ProviderResultCard
                 name="St. Austin's Day Care"
                 address="1234 Main St, Portland, OR 97201"
@@ -27,8 +34,48 @@ export default function SearchPage(props: SearchPageProps) {
                 phone="(503) 555-1234"
                 website="https://example.com"
               />
-            </div>
-          </ScrollArea>
+              <ProviderResultCard
+                name="St. Austin's Day Care"
+                address="1234 Main St, Portland, OR 97201"
+                serviceType="Housing"
+                description="A great place for kids to learn and grow."
+                phone="(503) 555-1234"
+                website="https://example.com"
+              />
+              <ProviderResultCard
+                name="St. Austin's Day Care"
+                address="1234 Main St, Portland, OR 97201"
+                serviceType="Housing"
+                description="A great place for kids to learn and grow."
+                phone="(503) 555-1234"
+                website="https://example.com"
+              />
+              <ProviderResultCard
+                name="St. Austin's Day Care"
+                address="1234 Main St, Portland, OR 97201"
+                serviceType="Housing"
+                description="A great place for kids to learn and grow."
+                phone="(503) 555-1234"
+                website="https://example.com"
+              />
+              <ProviderResultCard
+                name="St. Austin's Day Care"
+                address="1234 Main St, Portland, OR 97201"
+                serviceType="Housing"
+                description="A great place for kids to learn and grow."
+                phone="(503) 555-1234"
+                website="https://example.com"
+              />
+              <ProviderResultCard
+                name="St. Austin's Day Care"
+                address="1234 Main St, Portland, OR 97201"
+                serviceType="Housing"
+                description="A great place for kids to learn and grow."
+                phone="(503) 555-1234"
+                website="https://example.com"
+              />
+            </ScrollArea>
+          </div>
           <ServiceSearchBar className="z-10 ml-4 bg-white shadow-xl" orientation="horizontal" />
         </div>
       </ProviderMap>
