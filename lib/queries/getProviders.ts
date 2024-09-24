9e86dbf5ca7a907cfe3bff05a4a110d7c21dcfed
Payton Chertude <p.chertude34@gmail.com>
2024-09-23 20:45:40 -0700

    provider query

---
+++ lib/queries/getProviders.ts
@@ -0,0 +1,6 @@
+import { client } from "@/sanity/lib/client";
+import { groq } from "next-sanity";
+
+type SearchProvidersParamsType = {};
+
+export function searchProviders(params: SearchProvidersParamsType) {}
