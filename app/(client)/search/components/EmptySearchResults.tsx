020484ed895b5e542b61f58d397f83553ac4ef72
Payton Chertude <p.chertude34@gmail.com>
2024-09-26 07:11:50 -0700

    lighten empty result bg

--- app/(client)/search/components/EmptySearchResults.tsx
+++ app/(client)/search/components/EmptySearchResults.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export function EmptySearchResults() {
   return (
-    <div className="rounded-xl bg-slate-200 p-4">
+    <div className="rounded-xl bg-slate-100 p-4">
       <p>We could not find any results for your search criteria. Please modify your search and try again.</p>
     </div>
   );
