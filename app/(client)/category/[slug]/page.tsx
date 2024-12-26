acbe0b2885fff71bba27dec68c35b53c88bc0596
Payton Chertude <p.chertude34@gmail.com>
2024-12-26 09:11:04 -0800

    banenr

--- app/(client)/category/[slug]/page.tsx
+++ app/(client)/category/[slug]/page.tsx
@@ -12,12 +12,17 @@ export default async function CategoryPage(props: CategoryPageProps) {
   const serviceTypes = await queryServiceTypesByCategory(props.params.slug);
 
   return (
-    <>
+    <div>
+      {/* Banner */}
+      <div className="w-full bg-gradient-to-r from-primary-500 to-primary-800 py-10">
+        <h1 className="text-center text-5xl font-bold leading-relaxed text-white">{props.params.slug}</h1>
+      </div>
+      {/* Main Content */}
       <main className="container max-w-[1080px] space-y-5 py-8">
         {serviceTypes.map((serviceType) => (
           <CategoryDetailsCard label={serviceType.name} description="Test" count={1} />
         ))}
       </main>
-    </>
+    </div>
   );
 }
