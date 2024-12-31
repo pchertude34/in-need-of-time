c3c2c3deda80e3899a068cfd9c03ff1f7c7e91fc
Payton Chertude <p.chertude34@gmail.com>
2024-12-31 08:37:40 -0800

    add fancy arrows to hover

--- app/(client)/category/[slug]/components/CategoryDetailsCard.tsx
+++ app/(client)/category/[slug]/components/CategoryDetailsCard.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Link from "next/link";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import { cn } from "@/lib/utils";
 
 type CategoryDetailsCardProps = {
@@ -11,6 +14,7 @@ type CategoryDetailsCardProps = {
 
 export function CategoryDetailsCard(props: CategoryDetailsCardProps) {
   const { label, description, count, className } = props;
+  const [isHovering, setIsHovering] = useState(false);
 
   return (
     <Link
@@ -19,9 +23,22 @@ export function CategoryDetailsCard(props: CategoryDetailsCardProps) {
         "focus-ring-primary flex flex-col space-y-1.5 rounded-lg border-2 border-slate-200 bg-slate-50 transition-colors hover:border-primary-500 hover:bg-primary-50 focus:border-primary-500 focus:bg-primary-50 sm:p-6",
         className,
       )}
+      onMouseOver={() => setIsHovering(true)}
+      onMouseOut={() => setIsHovering(false)}
     >
       <div className="flex w-full items-center justify-between">
-        <h3 className="text-lg font-semibold">{label}</h3>
+        <div className="flex items-center">
+          <h3 className="text-lg font-semibold">{label}</h3>
+          <div
+            className={cn("ml-2 transform transition-all", {
+              "-translate-x-2 opacity-0": !isHovering,
+              "translate-x-0 opacity-100": isHovering,
+            })}
+          >
+            <ArrowRightIcon className="h-5 w-5 text-secondary-500" />
+          </div>
+        </div>
+
         {count && <span className="">{count}</span>}
       </div>
       <p className="text-slate-700">{description}</p>
