ce3d2510d6a64bc01a31ea5ef7d9bdf2e7fbc935
Payton Chertude <p.chertude34@gmail.com>
2026-03-31 06:17:26 -0700

    add missing shared functions

--- packages/shared/src/utils/index.ts
+++ packages/shared/src/utils/index.ts
@@ -1,3 +1,10 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
 const METERS_PER_MILE = 1609.34;
 
 export function convertMetersToMiles(meters: number) {
