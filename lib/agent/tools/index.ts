718eff66558a4ea643b263adbcbc05de50e57495
Payton Chertude <p.chertude34@gmail.com>
2026-04-07 07:06:14 -0700

    migration cleanup

--- lib/agent/tools/index.ts
+++
@@ -1,4 +0,0 @@
-import { getUrlContentToolDefinition } from "./getUrlContent";
-import { getServiceTypesToolDefinition } from "./getServiceTypes";
-
-export const tools = [getUrlContentToolDefinition, getServiceTypesToolDefinition];
