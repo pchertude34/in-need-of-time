3984f0a36e2c133a056ed3c6dd38d8a60c61b6a6
Payton Chertude <p.chertude34@gmail.com>
2026-04-13 07:21:00 -0700

    setup studio to run from .env.local locally

--- studio/src/lib/client.ts
+++ studio/src/lib/client.ts
@@ -1,9 +1,9 @@
 import { createClient } from "next-sanity";
 
-import { SANITY_API_VERSION, SANITY_STUDIO_DATASET, SANITY_STUDIO_PROJECT_ID, useCdn } from "../../env";
+import { SANITY_STUDIO_API_VERSION, SANITY_STUDIO_DATASET, SANITY_STUDIO_PROJECT_ID, useCdn } from "../../env";
 
 export const client = createClient({
-  apiVersion: SANITY_API_VERSION,
+  apiVersion: SANITY_STUDIO_API_VERSION,
   dataset: SANITY_STUDIO_DATASET,
   projectId: SANITY_STUDIO_PROJECT_ID,
   useCdn,
