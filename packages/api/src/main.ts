7f80b9364f0e2b25a62a4bcdeb4b445540b88bf0
Payton Chertude <p.chertude34@gmail.com>
2026-06-06 20:48:02 -0700

    initialize nestjs api

---
+++ packages/api/src/main.ts
@@ -0,0 +1,16 @@
+import "reflect-metadata";
+
+import { NestFactory } from "@nestjs/core";
+
+import { AppModule } from "./app.module";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix("api");
+
+  const port = Number(process.env.API_PORT || 4000);
+  await app.listen(port);
+}
+
+void bootstrap();
