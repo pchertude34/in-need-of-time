a5ccc1f40524d22bac96658244e4b1fe13c67901
Payton Chertude <p.chertude34@gmail.com>
2026-07-23 16:18:22 -0700

    fresh restart

--- packages/agent-core/src/tools/index.ts
+++
@@ -1,5 +0,0 @@
-export { getUrlContent, getUrlContentToolDefinition } from "./getUrlContent";
-
-import { getUrlContentToolDefinition } from "./getUrlContent";
-
-export const gatherWebsiteLinksTools = [getUrlContentToolDefinition];
--- packages/api/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-
-import { HealthController } from "./health.controller";
-import { UrlContentController } from "./url-content.controller";
-
-@Module({
-  controllers: [HealthController, UrlContentController],
-})
-export class AppModule {}
